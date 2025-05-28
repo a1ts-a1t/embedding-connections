768dcb00ecb3f1fe053dcbf274581a0e455680c9
alt! <unrelatedalt@gmail.com>
2025-05-27 23:55:03 -0700

    feat: add game data and fetch script

---
+++ scripts/load_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# dataset consts
+DATASET_URL="https://www.kaggle.com/api/v1/datasets/download/tm21cy/nyt-connections"
+DATASET_FILE_NAME="ConnectionsFinalDataset (1).json"
+
+# download dataset
+mkdir ./tmp
+curl -L -o ./tmp/game_data.zip $DATASET_URL
+unzip ./tmp/game_data.zip -d ./tmp
+
+# json transforms
+jq '[.[] | { words: .words, answer_key: [.answers[] | .words] }]' "./tmp/$DATASET_FILE_NAME" > ./data/game_data.json
+jq "[.[] | .words] | flatten | unique" ./data/game_data.json > ./data/all_words.json
+
+# cleanup
+rm -rf ./tmp
+
